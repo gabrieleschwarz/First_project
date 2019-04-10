07485b10acd5feb788866f801c7c623718d79004
G. Schwarz <48867632+gabrieleschwarz@users.noreply.github.com>
2019-04-11 00:15:14 +0200

    finished last problem

--- heart/Program.cs
+++ heart/Program.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace heart
 {
     class Program
-        
+
     {
         static void Main(string[] args)
         {
             #region Variable
             Console.Title = "Heart";
             // Variablen für Fragen
-            string[] fragen = new string [11];
+            string[] fragen = new string[11];
             fragen[0] = "Magst du Kaffee?";
             fragen[1] = "Ist Gabi super?";
             fragen[2] = "Magst du Affen?";
@@ -28,46 +28,7 @@ namespace heart
             fragen[10] = "Magst du Whos?";
             string J1 = "ja";
             string N1 = "nein";
-            // Rote Punkte
-
-            Coordinate Punkt;
-            Punkt.X = 14;
-            Punkt.Y = 10;
-            Coordinate Punkt1;
-            Punkt1.X = 15;
-            Punkt1.Y = 12;
-            Coordinate Punkt2;
-            Punkt2.X = 16;
-            Punkt2.Y = 8;
-            Coordinate Punkt3;
-            Punkt3.X = 17;
-            Punkt3.Y = 14;
-            Coordinate Punkt4;
-            Punkt4.X = 18;
-            Punkt4.Y = 10;
-            Coordinate Punkt5;
-            Punkt5.X = 19;
-            Punkt5.Y = 16;
-            Coordinate Punkt6;
-            Punkt6.X = 20;
-            Punkt6.Y = 10;
-            Coordinate Punkt7;
-            Punkt7.X = 21;
-            Punkt7.Y = 14;
-            Coordinate Punkt8;
-            Punkt8.X = 22;
-            Punkt8.Y = 8;
-            Coordinate Punkt9;
-            Punkt9.X = 23;
-            Punkt9.Y = 12;
-            Coordinate Punkt10;
-            Punkt10.X = 24;
-            Punkt10.Y = 10;
-
-            
-
-
-
+           
             //Spielfeld
             Console.SetWindowSize(40, 20);
             Console.SetBufferSize(40, 20);
@@ -80,57 +41,225 @@ namespace heart
 
             //Schleife
 
-            for (int i = 0; i < fragen.Length; i++)
-            {
+            
                 FragezeileAntwortzeile();
-
                 FrageAusgeben();
-
-                 
-                Console.Write(fragen[i]);
-                
-
+                Console.Write(fragen[0]);
                 // Verknüpfung frage antwort
-
                 Console.SetCursorPosition(32, 2);
-
                 string input = Console.ReadLine().ToLower();
-                if (input == J1 && i== 0 ||
-                    input == J1 && i == 1 ||
-                    input == N1 && i == 2 ||
-                    input == J1 && i == 3 ||
-                    input == N1 && i == 4 ||
-                    input == N1 && i == 5 ||
-                    input == J1 && i == 6 ||
-                    input == J1 && i == 7 ||
-                    input == J1 && i == 8 ||
-                    input == N1 && i == 9 ||
-                    input == N1 && i == 10 )
-                {
+                if (input == J1 )
+      
+                    {
+                        Console.SetCursorPosition(14, 10);
+                        RoterPunkt();
+                    }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
 
-                    Console.SetCursorPosition(14, 10);
+
+            //2
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[1]);
+            // Verknüpfung frage antwort
+            Console.SetCursorPosition(32, 2);
+            string input1 = Console.ReadLine().ToLower();
+
+            if (input1 == J1 )
+
+                {
+                    Console.SetCursorPosition(15, 12);
                     RoterPunkt();
                 }
-                
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+            //3
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[2]);
+            Console.SetCursorPosition(32, 2);
+            string input2 = Console.ReadLine().ToLower();
+            if (input2 == N1 )
+
+                {
+                    Console.SetCursorPosition(16, 8);
+                    RoterPunkt();
+                }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+            //4
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[3]);
+            Console.SetCursorPosition(32, 2);
+            string input3 = Console.ReadLine().ToLower();
+            if (input3 == J1)
+
+                {
+                    Console.SetCursorPosition(17, 14);
+                    RoterPunkt();
+                }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+            //5
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[4]);
+            Console.SetCursorPosition(32, 2);
+            string input4 = Console.ReadLine().ToLower();
+            if (input4 == N1 )
+
+                {
+                    Console.SetCursorPosition(18, 10);
+                    RoterPunkt();
+                }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+
+            //6
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[5]);
+            Console.SetCursorPosition(32, 2);
+            string input5 = Console.ReadLine().ToLower();
+            if (input5 == N1 )
+
+                {
+                    Console.SetCursorPosition(19, 16);
+                    RoterPunkt();
+                }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+
+            //7
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[6]);
+            Console.SetCursorPosition(32, 2);
+            string input6 = Console.ReadLine().ToLower();
+            if (input6 == J1 )
+
+                {
+                    Console.SetCursorPosition(20, 10);
+                    RoterPunkt();
+                }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+
+            //8
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[7]);
+            Console.SetCursorPosition(32, 2);
+            string input7 = Console.ReadLine().ToLower();
+            if (input7 == J1)
+
+                {
+                    Console.SetCursorPosition(21, 14);
+                    RoterPunkt();
+                }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+            //9
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[8]);
+            Console.SetCursorPosition(32, 2);
+            string input8 = Console.ReadLine().ToLower();
+            if (input8 == J1 )
+
+                {
+                    Console.SetCursorPosition(22, 8);
+                    RoterPunkt();
+                }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+            //10
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[9]);
+            Console.SetCursorPosition(32, 2);
+            string input9 = Console.ReadLine().ToLower();
+            if (input9 == N1)
+
+            {
+                Console.SetCursorPosition(23, 12);
+                RoterPunkt();
+            }
+            else
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Leider Nein");
+            }
+
+
+            //11
+
+            FragezeileAntwortzeile();
+            FrageAusgeben();
+            Console.Write(fragen[10]);
+            Console.SetCursorPosition(32, 2);
+            string input10 = Console.ReadLine().ToLower();
+            if (input10 == N1)
+
+                {
+                    Console.SetCursorPosition(24, 10);
+                    RoterPunkt();
+                }
 
                 else
                 {
                     Console.SetCursorPosition(0, 0);
                     Console.Write("Leider Nein");
                 }
-
-            }
-           
-
-
             
-            
-            
-            
-            
-            
-
-            Console.SetCursorPosition(7, 7);
+                //End
+                Console.SetCursorPosition(7, 7);
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Magenta;
 
@@ -138,13 +267,13 @@ namespace heart
             Console.Write(" ENDE ");
             Console.ReadKey();
             Console.CursorVisible = false;
-              
-            }
+        }
+
 
 
         static void FragezeileAntwortzeile()
         {  //Fragen zeile
-           
+
             Console.SetCursorPosition(5, 5);
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.Write("______________________________");
@@ -170,18 +299,17 @@ namespace heart
             Console.ForegroundColor = ConsoleColor.Red;
             Console.BackgroundColor = ConsoleColor.Red;
             Console.Write("_");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Gray;
         }
-        static void FindeneuePunktPosition()
-        {
-        }
+        
+        
+          
+
+      }
+
     }
 
-   struct Coordinate
-    {public int X;
-        public int Y;
-    }
-   
 
-}
 
 
